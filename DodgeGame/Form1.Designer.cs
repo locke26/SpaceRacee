a10971d7ce275f281dfbadeae28e3584f2a1a810
locke26 <90793969+locke26@users.noreply.github.com>
2022-06-03 14:01:26 -0400

    Add files via upload

---
+++ DodgeGame/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace DodgeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.p1 = new System.Windows.Forms.Label();
+            this.p2 = new System.Windows.Forms.Label();
+            this.timelabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // p1
+            // 
+            this.p1.BackColor = System.Drawing.Color.Transparent;
+            this.p1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p1.ForeColor = System.Drawing.Color.Transparent;
+            this.p1.Location = new System.Drawing.Point(12, 9);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(100, 23);
+            this.p1.TabIndex = 1;
+            this.p1.Text = "p1Score";
+            // 
+            // p2
+            // 
+            this.p2.BackColor = System.Drawing.Color.Transparent;
+            this.p2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p2.ForeColor = System.Drawing.Color.Transparent;
+            this.p2.Location = new System.Drawing.Point(472, 9);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(100, 23);
+            this.p2.TabIndex = 2;
+            this.p2.Text = "p2Score";
+            this.p2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // timelabel
+            // 
+            this.timelabel.BackColor = System.Drawing.Color.Transparent;
+            this.timelabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timelabel.ForeColor = System.Drawing.Color.Transparent;
+            this.timelabel.Location = new System.Drawing.Point(219, 7);
+            this.timelabel.Name = "timelabel";
+            this.timelabel.Size = new System.Drawing.Size(100, 23);
+            this.timelabel.TabIndex = 4;
+            this.timelabel.Text = "time";
+            this.timelabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.timelabel);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.p1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label p1;
+        private System.Windows.Forms.Label p2;
+        private System.Windows.Forms.Label timelabel;
+    }
+}
+
---
+++ DodgeGame/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace DodgeGame
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer deathplayer = new SoundPlayer(Properties.Resources._417486__mentoslat__8_bit_death_sound);
+        SoundPlayer pointplayer = new SoundPlayer(Properties.Resources._450614__breviceps__8_bit_collect_sound_timer_countdown);
+        
+        Rectangle player1 = new Rectangle(0, 300, 40, 10);
+        Rectangle player2 = new Rectangle(545, 300, 40, 10);
+        Rectangle point = new Rectangle(270, 280, 50, 50);
+       
+        int heroSpeed = 10;
+
+        List<Rectangle> balls = new List<Rectangle>();
+        List<int> ballSpeeds = new List<int>();
+        List<string> ballColours = new List<string>();
+        int ballSize = 10;
+
+        int p1score = 0;
+        int p2score = 0;
+        int time = 500;
+       
+        bool leftDown = false;
+        bool rightDown = false;
+        bool aDown = false;
+        bool dDown = false;
+
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush blackBrush = new SolidBrush(Color.Black);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+
+        Random randGen = new Random();
+        int randValue = 0;
+
+
+
+        int groundHeight = 50;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftDown = true;
+                    break;
+                case Keys.Right:
+                    rightDown = true;
+                    break;
+                case Keys.A:
+                    aDown = true;
+                    break;
+                case Keys.D:
+                    dDown = true;
+                    break;
+            }
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftDown = false;
+                    break;
+                case Keys.Right:
+                    rightDown = false;
+                    break;
+                case Keys.A:
+                    aDown = false;
+                    break;
+                case Keys.D:
+                    dDown = false;
+                    break;
+            }
+
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+           
+            if (dDown == true)
+            {
+                player1.X += heroSpeed;
+            }
+            if (aDown == true)
+            {
+                player1.X -= heroSpeed;
+            }
+
+            
+            if (rightDown == true)
+            {
+                player2.X += heroSpeed;
+            }
+            if (leftDown == true)
+            {
+                player2.X -= heroSpeed;
+            }
+
+          
+            for (int i = 0; i < balls.Count(); i++)
+            {
+               
+                int y = balls[i].Y + ballSpeeds[i];
+
+               
+                balls[i] = new Rectangle(balls[i].X, y, ballSize, ballSize);
+            }
+
+           
+            randValue = randGen.Next(0, 101);
+
+           
+            
+            if (randValue < 25) 
+            {
+                int x = randGen.Next(10, this.Width - ballSize * 2);
+                balls.Add(new Rectangle(x, 10, ballSize, ballSize));
+
+                ballSpeeds.Add(randGen.Next(2, 10));
+
+                ballColours.Add("ball");
+            }
+
+            for (int i = 0; i < balls.Count(); i++)
+            {
+
+                if (balls[i].Y > this.Height - ballSize - groundHeight)
+                {
+                    balls.RemoveAt(i);
+                    ballSpeeds.RemoveAt(i);
+                    ballColours.RemoveAt(i);
+                }
+            }
+            for (int i = 0; i < balls.Count(); i++)
+            {
+                if (player1.IntersectsWith(balls[i]))
+                {
+                    deathplayer.Play();
+                    player1.X = 0;
+                    player1.Y = 300;
+                    balls.RemoveAt(i);
+                    ballSpeeds.RemoveAt(i);
+                    ballColours.RemoveAt(i);
+                }
+                else if (balls[i].IntersectsWith(point))
+                {
+                    balls.RemoveAt(i);
+                    ballSpeeds.RemoveAt(i);
+                    ballColours.RemoveAt(i);
+                }
+                else if (player2.IntersectsWith(balls[i]))
+                {
+                    deathplayer.Play();
+                    player2.X = 545;
+                    player2.Y = 300;
+                    balls.RemoveAt(i);
+                    ballSpeeds.RemoveAt(i);
+                    ballColours.RemoveAt(i);
+                }
+                else if (player1.IntersectsWith(point))
+                {
+                    pointplayer.Play();
+                    p1score++;
+                    player1.X = 0;
+                    player1.Y = 300;
+                }
+                else if (player2.IntersectsWith(point))
+                {
+                    pointplayer.Play();
+                    p2score++;
+                    player2.X = 545;
+                    player2.Y = 300;
+                }
+                
+               
+            }
+
+            time--;
+
+            if (time == 0)
+            {
+                gameTimer.Enabled = false;
+            }
+
+            
+            
+            
+
+
+
+
+
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            
+
+            
+
+            p1.Text = $"Score: {p1score}";
+            p2.Text = $"Score: {p2score}";
+            timelabel.Text = $"Time: {time}";
+            
+
+            
+            e.Graphics.FillRectangle(blackBrush, 0, this.Height - groundHeight,
+
+                this.Width, groundHeight);
+
+            
+            e.Graphics.FillRectangle(whiteBrush, player1);
+            e.Graphics.FillRectangle(whiteBrush, player2);
+            e.Graphics.FillRectangle(redBrush, point);
+
+       
+
+            for (int i = 0; i < balls.Count(); i++)
+            {
+                if (ballColours[i] == "ball")
+                {
+                    e.Graphics.FillRectangle(whiteBrush, balls[i]);
+                }
+                
+            }
+
+            
+
+
+        }
+
+
+    }
+}
